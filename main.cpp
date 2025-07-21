28c18ef7452ad9da86484784a02c8a66e4728d32
X-WasHere <normanfediv@gmail.com>
2025-07-21 21:36:44 +0100

    Particle class rendering bouncy ball

--- main.cpp
+++ main.cpp
@@ -60,19 +60,14 @@ int main() {
 	glGenVertexArrays(1, &VAO); // create OpenGL obj of vertex array object
 	glGenBuffers(1, &VBO);      // "                 " vertex buffer object
 
-	// Wireframe mode
- /*   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);*/
 
-	// Screen scaler
-	float scale = 10.0f;
-
+	// Particle properties
 	std::vector<float> position = { 0.0f, 0.0f , 0.0f};
-	std::vector<float> velocity = { 0.0f, 0.0f , 0.0f};
 	float radius = 50.0f;
 	int numSides = 50;
-
 	float dampingFactor = 0.85f;
 
+	// Particle object
 	Particle particle(position, radius, numSides, ourShader, SCREEN_WIDTH, SCREEN_HEIGHT);
 
 	// RENDERING LOOP
@@ -90,24 +85,8 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
 
 		particle.drawCircle(VAO, VBO);
-		//// Draw particle 
-		//drawCircle(VBO, VAO, position, radius, numSides, ourShader);
-
-		//// Update position
-		//position[0] += velocity[0] * deltaTime;
-		//position[1] += velocity[1] * deltaTime;
-		//velocity[1] += (-9.81f) * deltaTime; // scale velocity for screen coord {-1,-1}
-
-		//// Add border collision
-		//if (position[1] - (radius / SCREEN_HEIGHT) <= -1.0) // check bottom of screen 
-		//{
-		//	position[1] = -1.0 + (radius / SCREEN_HEIGHT); // prevent bounce from below floor
-		//	velocity[1] *= -1.0 * dampingFactor;
-
-		//	if (std::abs(velocity[1]) < 0.1f) {
-		//		velocity[1] = 0.0f;
-		//	}
-		//}
+		particle.add_velocity(deltaTime);
+		particle.add_border_collision(dampingFactor);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents(); // check for event triggering
@@ -116,43 +95,6 @@ int main() {
 	glfwTerminate();
 	return 0;
 }
-
-//// Draw circle
-//void drawCircle(unsigned int& VBO, unsigned int& VAO, std::vector<float> position, 
-//	float radius, int numSides, Shader& ourShader) 
-//{
-//	int numVertices = numSides + 2; 
-//
-//	float doublePi = 2.0f * 3.141592653f;
-//	
-//	// Setup verticies vector
-//	std::vector<float> vertices;
-//	vertices.push_back(position[0]); // x
-//	vertices.push_back(position[1]); // y
-//	vertices.push_back(position[2]); // z
-//	// Trigonometry to calculate vertices (rememeber 0,0 is centre) 
-//	for (int i = 1; i < numVertices; i++) {
-//		vertices.push_back(position[0] + (radius * cos(i * doublePi / numSides)) / SCREEN_WIDTH); // remember that screen dimensions are normalised
-//		vertices.push_back(position[1] + (radius * sin(i * doublePi / numSides)) / SCREEN_HEIGHT); // verticies must be {-1, 1}
-//		vertices.push_back(position[2]);
-//	}
-//
-//	glBindVertexArray(VAO);
-//
-//	// Bind VBO buffer
-//	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-//	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW); // copy vertex data into buffer
-//
-//	// Set vertex attribute pointers 
-//	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-//	glEnableVertexAttribArray(0);
-//
-//	// Draw objects 
-//	ourShader.use(); // activate shader
-//	glDrawArrays(GL_TRIANGLE_FAN, 0, numVertices);
-//	glBindVertexArray(0);
-//
-//}
 
 void updatePosition(std::vector<float>position, std::vector<float>velocity, float deltaTime) 
 {
--- particle_class.h
+++ particle_class.h
@@ -11,38 +11,40 @@ class Particle
 public:
 	std::vector<float> position;
 	std::vector<float> vertices;
+	std::vector<float> velocity = { 0.0f, 0.0f, 0.0f };
 	unsigned int numSides;
 	int numVertices;
 	float radius;
 	
-	Shader* ourShader;
+	Shader ourShader;
 
 	unsigned int SCR_WIDTH;
 	unsigned int SCR_HEIGHT;
 
-	// Constructor sets up circle vertices
-	Particle(std::vector<float> position_, float radius_, int numSides_, Shader* ourShader_, const unsigned int SCREEN_WIDTH, const unsigned SCREEN_HEIGHT) 
+	// Constructor sets up circle vertices vector with initial xyz position
+	Particle(std::vector<float> position_, float radius_, int numSides_, Shader& ourShader_, const unsigned int SCREEN_WIDTH, const unsigned SCREEN_HEIGHT) 
 		: position(position_), radius(radius_), numSides(numSides_), ourShader(ourShader_), SCR_WIDTH(SCREEN_WIDTH), SCR_HEIGHT(SCREEN_HEIGHT)
 	{
 		numVertices = numSides + 2;
-
-		float doublePi = 2.0f * 3.141592653f;
 
 		// Setup verticies vector
 		vertices.push_back(position[0]); // x
 		vertices.push_back(position[1]); // y
 		vertices.push_back(position[2]); // z
-		// Trigonometry to calculate vertices (rememeber 0,0 is centre) 
-		for (int i = 1; i < numVertices; i++) {
-			vertices.push_back(position[0] + (radius * cos(i * doublePi / numSides)) / SCREEN_WIDTH); // remember that screen dimensions are normalised
-			vertices.push_back(position[1] + (radius * sin(i * doublePi / numSides)) / SCREEN_HEIGHT); // verticies must be {-1, 1}
-			vertices.push_back(position[2]);
-		}
 	}
 
 	// Member functions 
 	void drawCircle(unsigned int& VAO, unsigned int& VBO)
 	{
+		float doublePi = 2.0f * 3.141592653f;
+		vertices.clear();
+		// Trigonometry to calculate vertices (rememeber 0,0 is centre) 
+		for (int i = 1; i < numVertices; i++) {
+			vertices.push_back(position[0] + (radius * cos(i * doublePi / numSides)) / SCR_WIDTH); // remember that screen dimensions are normalised
+			vertices.push_back(position[1] + (radius * sin(i * doublePi / numSides)) / SCR_HEIGHT); // verticies must be {-1, 1}
+			vertices.push_back(position[2]);
+		}
+
 		glBindVertexArray(VAO);
 
 		// Bind VBO buffer
@@ -54,9 +56,30 @@ public:
 		glEnableVertexAttribArray(0);
 
 		// Draw objects 
-		(*ourShader).use(); // activate shader
+		ourShader.use(); // activate shader
 		glDrawArrays(GL_TRIANGLE_FAN, 0, numVertices);
 		glBindVertexArray(0);
+	}
+
+	void add_velocity(float deltaTime)
+	{
+		// Update position
+		position[0] += velocity[0] * deltaTime;
+		position[1] += velocity[1] * deltaTime;
+		velocity[1] += (-9.81f) * deltaTime; 
+	}
+
+	void add_border_collision(float dampingFactor)
+	{
+		if (position[1] - (radius / SCR_HEIGHT) <= -1.0) // check bottom of screen 
+		{
+			position[1] = -1.0 + (radius / SCR_HEIGHT); // prevent bounce from below floor
+			velocity[1] *= -1.0 * dampingFactor;
+
+			if (std::abs(velocity[1]) < 0.1f) {
+				velocity[1] = 0.0f;
+			}
+		}
 	}
 };
 
